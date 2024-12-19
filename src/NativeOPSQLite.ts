41efd964d5d49134b31409585f14db0e89f4ac79
zy432099 <46698434+zy432099@users.noreply.github.com>
2024-12-19 15:43:10 +0800

    开源五项核查 (#10)
    Co-authored-by: 张宇 <478110507@qq.com>

--- src/NativeOPSQLite.ts
+++ src/NativeOPSQLite.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import { TurboModuleRegistry, type TurboModule } from 'react-native';
 
 export interface Spec extends TurboModule {
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import { NativeModules, TurboModuleRegistry } from 'react-native';
 
 const NativeOPSQLite = TurboModuleRegistry.getEnforcing('OPSQLite') ? TurboModuleRegistry.getEnforcing('OPSQLite') : NativeModules.OPSQLite;
